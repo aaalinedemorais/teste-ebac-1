e40a54e12f93920d062d957cd27b5d5206bdd00d
Aline Morais <morais.alinede@gmail.com>
2026-01-08 16:31:04 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,12 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('[name="username"]').type('amora@uorak.com')
+        cy.get('.woocommerce-form > :nth-child(2) > [name="password"]').type('teste123')
+        cy.get('[name="login"]').click()
+       cy.get('.woocommerce-MyAccount-content').should('contain', 'A partir do painel de controle de sua conta')
+    })
+})
