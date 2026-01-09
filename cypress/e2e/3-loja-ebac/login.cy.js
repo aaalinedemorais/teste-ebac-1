a898bd2c639f25a08faf8a55426c1e376e1cc76b
aaalinedemorais <aline.morais@altoqi.com.br>
2026-01-09 08:47:56 -0300

    add cenário de teste

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,6 +2,13 @@
 
 describe('Funcionalidade: Login', () => {
 
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('[name="username"]').type('amora@uorak.com')
+        cy.get('.woocommerce-form > :nth-child(2) > [name="password"]').type('teste123')
+        cy.get('[name="login"]').click()
+    });
+
     it('Deve fazer login com sucesso', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('[name="username"]').type('amora@uorak.com')
@@ -9,4 +16,5 @@ describe('Funcionalidade: Login', () => {
         cy.get('[name="login"]').click()
        cy.get('.woocommerce-MyAccount-content').should('contain', 'A partir do painel de controle de sua conta')
     })
+    
 })
